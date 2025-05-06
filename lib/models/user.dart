6ea6ddce8fb6fba21546efdfc82413fae79d5b4b
finn1817 <finn1817@fredonia.edu>
2025-05-06 01:21:12 -0400

    updating user model comments explaining what does what

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,11 +1,21 @@
 // lib/models/user.dart
+// model we made for the users stored in Firebase db (at our 'users' collection)
 
 class User {
+  // unique ID (same as the Firebase doc ID)
   final String id;
+
+  // username used for login
   final String username;
-  final String password;    // plaintext for now
+
+  // password (plain text as default sets to guest non-admin user for testing,
+  // but the passwords and users are stored on firebase)
+  final String password;
+
+  // true if user is an admin (1) or false if user is not (0)
   final bool isAdmin;
 
+  // constructor to make the User object
   User({
     required this.id,
     required this.username,
@@ -13,20 +23,22 @@ class User {
     required this.isAdmin,
   });
 
+  // creates a User from Firebase data + doc ID
   factory User.fromMap(String id, Map<String, dynamic> data) {
     return User(
-      id:       id,
+      id: id,
       username: data['username'] as String,
       password: data['password'] as String,
-      isAdmin:  (data['isAdmin'] as int) == 1,
+      isAdmin: (data['isAdmin'] as int) == 1, // stored as 1 or 0
     );
   }
 
+  // converts this User into a map for Firebase
   Map<String, dynamic> toMap() {
     return {
       'username': username,
       'password': password,
-      'isAdmin':  isAdmin ? 1 : 0,
+      'isAdmin': isAdmin ? 1 : 0, // stored as int in Firebase, not a string
     };
   }
 }
